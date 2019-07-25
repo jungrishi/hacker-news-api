578dbb7472f036ddb9508680a0224bd6786c4506
rishiJung <you@example.com>
2019-07-25 11:16:15 +0545

    add css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import Story from './component/Story';
 import ListStory from './component/ListStory';
+import './styles/listing.css';
 
 
 
@@ -11,7 +12,12 @@ function App() {
   return (
     <div className="App">
       <>
-        <h1>Hacker News</h1>
+      <h1>Header</h1>
+        {/* <h1><a><Link
+                        to={{
+                            pathname: `/`}}>Hacker News        
+                    </Link>  
+                  </a>    </h1> */}
         <Router>
           <Route exact path="/" component={ListStory}/>
           <Route exact path="/stories/:storyId" component={Story} />
