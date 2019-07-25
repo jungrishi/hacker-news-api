b8fec4dd9574c09ce1f9da56d9a25fec21c7c712
rishiJung <you@example.com>
2019-07-25 11:35:04 +0545

    Updates

---
+++ precache-manifest.52cc348541ebdf2a4c373b27f20a638d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b57146fdbe5d044e1301ad8f0a357e1",
+    "url": "/hacker-news-api/index.html"
+  },
+  {
+    "revision": "fe89d55677d5f14432ec",
+    "url": "/hacker-news-api/static/css/main.d0219089.chunk.css"
+  },
+  {
+    "revision": "c69cf491cc3c4770d9c6",
+    "url": "/hacker-news-api/static/js/2.adcd7173.chunk.js"
+  },
+  {
+    "revision": "fe89d55677d5f14432ec",
+    "url": "/hacker-news-api/static/js/main.bf8a79ee.chunk.js"
+  },
+  {
+    "revision": "8df2ffe9203b4ba5a436",
+    "url": "/hacker-news-api/static/js/runtime~main.51e5e80e.js"
+  }
+]);
